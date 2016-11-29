d2c6f838f52b8356dcb32e79ba6aa9223fece3b1
stacksmith <stack@apple2.x10.mx>
2016-11-29 12:09:29 -0800

    exporting correct symbols

--- package.lisp
+++ package.lisp
@@ -5,7 +5,30 @@
   (:use #:cl)
   (:export :connect
 	   :disconnect
-	   :send-command
-	   :read-list
-	   :read-unit))
+	   :command
+	   :range
+	   :rlist
+	   :rline
+	   :reconnect
+	   :nntp-error
+	   :server
+	   :make-server
+	   :server-name
+	   :server-port
+	   :server-user
+	   :server-password
+	   :server-ssl
+	   :name
+	   :port
+	   :user
+	   :password
+	   :ssl
+	   :conn
+	   :make-conn
+	   :conn-server
+	   :conn-group
+	   :conn-stream
+	   :conn-bytes-read
+	   :group
+	   :bytes-read))
 
